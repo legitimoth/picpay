39d9c4a7da6199bbcf575457254217e4483b7a0f
Thaynner Bastos <cast.thaynner.utic@sebrae.com.br>
2023-10-14 13:01:35 -0300

    🚧 WIP: Criando repositorio do usuário e interface

---
+++ Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace picpay;
+
+public interface IUserRepository
+{
+    public Task<List<User>> GetAll();
+    public Task<User> GetById();
+    public Guid Create();
+    public void Update(User user);
+    public Task Delete(Guid Id);
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿
+namespace picpay;
+
+public class UserRepository : IUserRepository
+{
+    public Guid Create()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<User>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetById()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
