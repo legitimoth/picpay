c1b41d63d1680c4758772f1d7183c36667736710
Thaynner Bastos <cast.thaynner.utic@sebrae.com.br>
2023-10-14 13:23:48 -0300

    ✅ Done: Criado DbContext e instalado pacotes.

--- Entities/User.cs
+++ Entities/User.cs
@@ -3,10 +3,27 @@
 public class User
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Document { get; set; }
-    public decimal Wallet { get; set; }
-    public UserType Type { get; set; }
-    public bool Active { get; set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Document { get; private set; }
+    public decimal Wallet { get; private set; }
+    public UserType Type { get; private set; }
+    public bool Active { get; private set; }
+
+    public User(
+        string firstName,
+        string lastName,
+        string document,
+        decimal wallet,
+        UserType type,
+        Guid Id = default
+    )
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Document = document;
+        Wallet = wallet;
+        Type = type;
+        Active = true;
+    }
 }
---
+++ Repositories/PicPayDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace picpay;
+
+public class PicPayDbContext : DbContext
+{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=./Data/picpay.db");
+        }
+
+        public DbSet<User> Users { get; set; }
+}
