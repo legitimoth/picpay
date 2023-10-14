839cff4c7f3cd8e12f7ec6bedeedcd448fa11b6f
Thaynner Bastos <cast.thaynner.utic@sebrae.com.br>
2023-10-14 12:48:36 -0300

    ✅ Done: Adicionado Usuário.

---
+++ Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace picpay;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Document { get; set; }
+    public decimal Wallet { get; set; }
+    public UserType Type { get; set; }
+    public bool Active { get; set; }
+}
