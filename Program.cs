ca22250d18bec18cfcb01c55ec5e66871c7c4b53
Thaynner Bastos <cast.thaynner.utic@sebrae.com.br>
2023-10-14 23:24:02 -0300

    ✅ Done: Adicionado docker / docker-compose.

--- Program.cs
+++ Program.cs
@@ -37,12 +37,9 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
